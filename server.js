49fa20f68e7748b3fcf2c5bde142b08689d4b809
SakifKhan98 <sakifkhan98@gmail.com>
2020-10-24 03:18:00 +0600

    Added Database

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("This Epic shit Really running !!!");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`.magenta
+      .bold
+  )
 );
